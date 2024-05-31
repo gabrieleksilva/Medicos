6b47fdf1c7bd1c14ec8272a1ae624332afad3d1c
Gabriele K Silva <gabriele.s@aluno.ifsp.edu.br>
2024-05-30 23:47:15 -0300

    configuracao de liberacao de usuario e BCryptPassword

---
+++ src/main/java/br/edu/ifsp/pw3/api/usuario/DadosAutenticacao.java
@@ -0,0 +1,11 @@
+package br.edu.ifsp.pw3.api.usuario;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record DadosAutenticacao(
+        @NotBlank
+        String login,
+        @NotBlank
+        String senha
+) {
+}
---
+++ src/main/resources/db/migration/V5__insere_asdrubal.sql
@@ -0,0 +1,2 @@
+INSERT INTO usuarios(login, senha)
+VALUES ('asdrubal@gmail.com', '$2a$12$6sMhh6qHhPtm41NGGMLLnuEe/IqAbGFJo6sK6CRFs0k.10zhVTJXG');
